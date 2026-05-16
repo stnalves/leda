f3322275a2cb2cac311b0199376ac874e2a6716a
Stefany <stefany.santos2302@gmail.com>
2026-05-16 10:16:38 -0300

    ajustes

--- tst/counting/radix/Teste.java
+++
@@ -1,9 +0,0 @@
-class Teste {
-    public static void main(String[] args) {
-        int n = 8936;
-
-        for (int exp = 1; n / exp > 0; exp *= 10) {
-            System.out.println((n / exp) % 10);
-        }
-    }
-}
